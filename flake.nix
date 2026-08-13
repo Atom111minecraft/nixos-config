4a6916bb7c04555ff15a4f1949ff9453f72fccf6
Atom111minecraft <Atom111minecraft@users.noreply.github.com>
2026-08-13 19:55:03 +0300

    Add zapret with general (EXP) strategy

--- flake.nix
+++ flake.nix
@@ -4,13 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     noctalia.url = "github:noctalia-dev/noctalia";
+    zapret-discord-youtube.url = "github:kartavkun/zapret-discord-youtube";
   };
 
-  outputs = { nixpkgs, noctalia, ... }: {
+  outputs = { nixpkgs, noctalia, zapret-discord-youtube, ... }: {
     nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         noctalia.nixosModules.default
+          zapret-discord-youtube.nixosModules.withTestTools
+          {
+            services.zapret-discord-youtube.enable = true;
+            services.zapret-discord-youtube.configName = "general (EXP)";
+          }
         ./nixos/configuration.nix
       ];
     };
