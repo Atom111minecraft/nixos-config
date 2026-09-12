ef8003b829fec48ffcc38237631e7cbf4eed56df
Atom111minecraft <Atom111minecraft@users.noreply.github.com>
2026-09-12 16:43:45 +0300

    OpenShot: fix playback breakage (SDL video/audio drivers)

--- nixos/modules/openshot.nix
+++ nixos/modules/openshot.nix
@@ -5,7 +5,10 @@ let
     paths = [ pkgs.openshot-qt ];
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
-      wrapProgram "$out/bin/openshot-qt" --set QT_QPA_PLATFORM xcb
+      wrapProgram "$out/bin/openshot-qt" \
+        --set QT_QPA_PLATFORM xcb \
+        --set SDL_VIDEODRIVER x11 \
+        --set SDL_AUDIODRIVER pulseaudio
     '';
   };
 in
