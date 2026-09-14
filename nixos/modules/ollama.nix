2efb103cbfe2c67bd13e0b1d20b5fa9d3cdbd129
Atom111minecraft <Atom111minecraft@users.noreply.github.com>
2026-09-14 16:43:58 +0300

    Add Python 3.12 for uv compatibility

--- nixos/modules/ollama.nix
+++ nixos/modules/ollama.nix
@@ -5,7 +5,7 @@
     package = pkgs.ollama-cuda;
   };
 
-  environment.systemPackages = [ pkgs.uv pkgs.rustc pkgs.cargo pkgs.gcc pkgs.binutils ];
+  environment.systemPackages = [ pkgs.uv pkgs.rustc pkgs.cargo pkgs.gcc pkgs.binutils pkgs.python312 ];
   nix.settings.max-jobs = 12;
   nix.settings.cores = 12;
 }
