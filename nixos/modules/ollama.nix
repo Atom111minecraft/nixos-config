5b828e02083fba449283df3e34e73a28c1a3b663
Atom111minecraft <Atom111minecraft@users.noreply.github.com>
2026-09-14 16:40:49 +0300

    Add gcc and binutils for Rust C-linker support

--- nixos/modules/ollama.nix
+++ nixos/modules/ollama.nix
@@ -5,7 +5,7 @@
     package = pkgs.ollama-cuda;
   };
 
-  environment.systemPackages = [ pkgs.uv pkgs.rustc pkgs.cargo ];
+  environment.systemPackages = [ pkgs.uv pkgs.rustc pkgs.cargo pkgs.gcc pkgs.binutils ];
   nix.settings.max-jobs = 12;
   nix.settings.cores = 12;
 }
