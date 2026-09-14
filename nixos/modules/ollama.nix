6c591696ff33368239f5611e2dfe8799d68cb4a7
Atom111minecraft <Atom111minecraft@users.noreply.github.com>
2026-09-14 16:36:20 +0300

    Add Rust compiler for Python package builds

--- nixos/modules/ollama.nix
+++ nixos/modules/ollama.nix
@@ -5,7 +5,7 @@
     package = pkgs.ollama-cuda;
   };
 
-  environment.systemPackages = [ pkgs.uv ];
+  environment.systemPackages = [ pkgs.uv pkgs.rustc pkgs.cargo ];
   nix.settings.max-jobs = 12;
   nix.settings.cores = 12;
 }
