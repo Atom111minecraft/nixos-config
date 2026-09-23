69dc8c323e570ef1ea4773377262004664712725
Atom111minecraft <Atom111minecraft@users.noreply.github.com>
2026-09-23 17:25:14 +0300

    Fix Sober: pass input to outputs function

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     sober.url = "github:vinegarhq/sober";
   };
 
-  outputs = { nixpkgs, noctalia, zapret-discord-youtube, ... }: {
+  outputs = { nixpkgs, noctalia, zapret-discord-youtube, sober, ... }: {
     nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
