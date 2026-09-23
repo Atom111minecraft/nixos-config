843ebc57dcb9075dd26713c868fd920b99cbeac6
Atom111minecraft <Atom111minecraft@users.noreply.github.com>
2026-09-23 20:54:18 +0300

    Replace ydotool with Flatpak for XClicker autoclicker

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,8 +2,7 @@
 
 {
   imports = [
-    ./modules/autoclicker.nix
-    ./modules/autoclicker.nix
+    ./modules/flatpak.nix
     ./modules/steam.nix
     ./modules/ollama.nix
     ./hardware-configuration.nix
